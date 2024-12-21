9c95a28bfe56082578a3b6c9e487f44420e59fda
JSanchoNoit <von.sancho@gmail.com>
2024-12-21 15:24:20 +0100

    Iniciado proyecto JS.

---
+++ src/js/Practica_Final_ECMAScript.js
@@ -0,0 +1,5 @@
+// TODO: Asegúrate de que los apuntes confirman esta solución. 
+sessionStorage.setItem("practica", "Práctica Final ECMAScript 2025");
+
+// Comprobar que se ha almacenado correctamente
+console.log(sessionStorage.getItem("practica"));
