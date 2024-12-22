2216b08274a8e3996f81e8f4d35c953aeb77a366
James Neutron <von.sancho@gmail.com>
2024-12-22 09:51:04 +0100

    Trabajo realizado hasta 21DIC.

--- src/js/Practica_Final_ECMAScript.js
+++ src/js/Practica_Final_ECMAScript.js
@@ -1,5 +1,23 @@
+// Ejercicio 2
+
 // TODO: Asegúrate de que los apuntes confirman esta solución. 
 sessionStorage.setItem("practica", "Práctica Final ECMAScript 2025");
 
 // Comprobar que se ha almacenado correctamente
 console.log(sessionStorage.getItem("practica"));
+
+// Ejercicio 3
+const devolverHora = function () {
+    const today = new Date();
+    let hora = today.getHours();
+    let minuto = today.getMinutes();
+    let segundo = today.getSeconds();
+
+    return `${hora}:${minuto}:${segundo}`
+};
+
+console.log("Ejercicio 3 ==> " + devolverHora());
+
+// Ejercicio 4
+const relojCabecera = document.querySelector('#relojDigital');
+relojCabecera.insertAdjacentText(0, "Hola");
