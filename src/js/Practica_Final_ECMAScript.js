0ece1489ace40eae7517e7bb075111af3267d782
JD.Sancho <von.sancho@gmail.com>
2024-12-22 11:53:45 +0100

    Hasta el ejercicio 6 incluido.

--- src/js/Practica_Final_ECMAScript.js
+++ src/js/Practica_Final_ECMAScript.js
@@ -20,6 +20,30 @@ console.log("Ejercicio 3 ==> " + devolverHora());
 
 // Ejercicio 4. Nos damos cuenta que hay que introducir un "Defer" para que el HTML se cargue antes que el JS.
 // TODO: Que el reloj se actualice cada segundo.
-console.log("Ejercicio 4 ==>");
-let relojCabecera = document.getElementById('relojDigital');
+// TODO: Reevaluar si la mejor solución es un innerText
+const relojCabecera = document.getElementById('relojDigital');
 relojCabecera.innerText = devolverHora();
+
+// Ejercicio 5
+// TODO: Checkear si otro método es correcto (esto añade estilo en línea, pero no modifica el documento CSS)
+const titulo = document.getElementById('rotuloJugandoConMayores');
+
+// Función que cambia el color del título al pasar el ratón
+titulo.addEventListener('mouseover', () => {
+    titulo.style.color = '#ffff00';  // Cambia a un color azul
+});
+
+// Función que devuelve el color original cuando el ratón sale
+titulo.addEventListener('mouseout', () => {
+    titulo.style.color = '';  // Vuelve al color original (negro)
+});
+
+// Ejercicio 6
+let fotoCentralMain = document.getElementById('fotoCentralMain');
+fotoCentralMain.addEventListener('click', function() {
+    if (fotoCentralMain.src.includes('public/images/mayoresJugando01.jpg')) {
+        fotoCentralMain.src = 'public/images/ImagenEjercicio6.jpg';
+    } else {
+        fotoCentralMain.src = 'public/images/mayoresJugando01.jpg';
+    }
+});
